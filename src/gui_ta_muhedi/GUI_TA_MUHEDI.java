f3a448bc35cfac8bfb0954406f4172fd0db8138b
muhedi <muhedi.me@gmail.com>
2019-05-23 23:29:24 +0700

    TA DKP 2019

---
+++ src/gui_ta_muhedi/GUI_TA_MUHEDI.java
@@ -0,0 +1,27 @@
+
+package gui_ta_muhedi;
+
+public class GUI_TA_MUHEDI {
+    String isi1, isi2, hasil2;
+    public void sethasil2(String hasil2) {
+        this.hasil2 = hasil2;
+    }
+    public String gethasil2(){
+        return hasil2;
+    }
+    public static void main(String[] args) {
+         MUHEDI utama = new MUHEDI();
+    utama.setVisible(true);
+    }
+
+    String sethasil() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    String sethasil2() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    }
+
+    
+
